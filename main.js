86e1636375b7eec52052bfafb69aa3d8cb9acfdb
kyrylomukha <111917832+kyrylomukha@users.noreply.github.com>
2024-03-26 20:08:31 +0200

    Linked UI elements, added basic math functions

---
+++ main.js
@@ -0,0 +1,48 @@
+ // UI
+
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
+
+const add = document.querySelector("#add");
+const subtract = document.querySelector("#subtract");
+const divide = document.querySelector("#divide");
+const multiply = document.querySelector("#multiply");
+const point = document.querySelector("#point");
+
+const clear = document.querySelector("#clear");
+const del = document.querySelector("#delete");
+
+const result = document.querySelector("#operand");
+
+
+ function addition(a,b) {
+  return a + b;
+ }
+
+  function subtraction(a,b) {
+  return a - b;
+ }
+
+  function multiplication(a,b) {
+  return a * b;
+ }
+
+  function division(a,b) {
+  return a / b;
+ }
+
+
+/*one.addEventListener("click", () => {
+  result.textContent = "1";
+ }); */
+ 
+ 
+ 
